4d6f0905c7f7cf818d1e6ec9a9a785cde87e1bd9
GaborWilk <48469432+GaborWilk@users.noreply.github.com>
2019-09-25 20:41:39 +0200

    Add files via upload

---
+++ include/PrintTeams.h
@@ -0,0 +1,14 @@
+#ifndef PRINT_TEAMS_H
+#define PRINT_TEAMS_H
+
+#include "Resources.h"
+
+class PrintTeams {
+    public:
+        void printTeams(res::playerStatistics&& teamA, double avgOfTeamA, res::playerStatistics&& teamB, double avgOfTeamB) const;
+
+    private:
+        void printPlayers(res::playerStatistics&& player) const;
+};
+
+#endif // PRINT_TEAMS_H
