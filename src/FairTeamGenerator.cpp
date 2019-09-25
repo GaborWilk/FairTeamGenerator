a89fcbb42c11d61179c1055d34e807706e25ef60
GaborWilk <48469432+GaborWilk@users.noreply.github.com>
2019-09-25 20:45:40 +0200

    Update FairTeamGenerator.cpp

--- src/FairTeamGenerator.cpp
+++ src/FairTeamGenerator.cpp
@@ -9,52 +9,44 @@ namespace ftg {
 
     using namespace res;
 
-    void FairTeamGenerator::setNumOfPlayers(unsigned short num)
-    {
+    void FairTeamGenerator::setNumOfPlayers(unsigned short num) {
         numOfPlayers_ = num;
     }
 
-    unsigned short FairTeamGenerator::getNumOfPlayers() const
-    {
+    unsigned short FairTeamGenerator::getNumOfPlayers() const {
         return numOfPlayers_;
     }
 
-    void FairTeamGenerator::setPlayerRating(const std::string& name, unsigned int rating)
-    {
+    void FairTeamGenerator::setPlayerRating(const std::string& name, unsigned int rating) {
         players_.emplace_back(std::make_pair(name, rating));
     }
 
-    auto FairTeamGenerator::getPlayerRatingByName(const std::string& name) const
-    {
-        unsigned int res = 0;
+    auto FairTeamGenerator::getPlayerRatingByName(const std::string& name) const {
+        unsigned int rating = 0;
 
-        for(auto& it : players_) {
-            if(name == it.first) {
-                res = it.second;
+        for (auto& it : players_) {
+            if (name == it.first) {
+                rating = it.second;
                 break;
             }
         }
 
-        return res;
+        return rating;
     }
 
-    double FairTeamGenerator::getAverageRatingOfTeamA() const
-    {
+    double FairTeamGenerator::getAverageRatingOfTeamA() const {
         return algorithm_->getRatingOfTeamA();
     }
 
-    double FairTeamGenerator::getAverageRatingOfTeamB() const
-    {
+    double FairTeamGenerator::getAverageRatingOfTeamB() const {
         return algorithm_->getRatingOfTeamB();
     }
 
-    void FairTeamGenerator::setAlgorithmType(std::unique_ptr<AbstractAlgorithm> ptr)
-    {
+    void FairTeamGenerator::setAlgorithmType(std::unique_ptr<AbstractAlgorithm> ptr) {
         algorithm_ = std::move(ptr);
     }
 
-    void FairTeamGenerator::useAlgorithm()
-    {
+    void FairTeamGenerator::useAlgorithm() {
         algorithm_->calculateFairTeams(std::move(players_));
     }
 
